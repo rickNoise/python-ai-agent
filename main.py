2170813cfecd6bf5734ef583edbaec7d4890886b
rickNoise <emberix@gmail.com>
2025-08-20 13:24:57 +1000

    allow cli argument to pass llm prompt

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
-import os
+import os, sys
 from dotenv import load_dotenv
 from google import genai
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-
-# create a new instance of a Gemini client
-# https://googleapis.github.io/python-genai/#create-a-client
-client = genai.Client(api_key=api_key)
 
 def main():
+    load_dotenv()
+    
+    if len(sys.argv) < 2:
+        print("usage: python main.py \"<LLM PROMPT>\"")
+        sys.exit(1)
+    user_prompt = sys.argv[1]
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    # create a new instance of a Gemini client
+    # https://googleapis.github.io/python-genai/#create-a-client
+    client = genai.Client(api_key=api_key)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001', 
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.',
+        contents=user_prompt,
     )
 
     print("Response:")
